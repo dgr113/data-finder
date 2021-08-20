60c4cb249625e08b56223061d7824f893f0d98e6
d.grigoriev <dmitry-gr87@yandex.ru>
2021-08-20 19:25:58 +0300

    Slightly refactoring and update dependencies

--- src/core/config_utils.rs
+++ src/core/config_utils.rs
@@ -3,9 +3,10 @@ use serde_yaml;
 
 
 
-/// Load config ///
-pub fn load_config(path: &str) -> serde_yaml::Value {
-    let config_str = fs::read_to_string(path).expect("Not config file!");
-    let config = serde_yaml::from_str(&config_str).unwrap();
+
+/** Load config */
+pub fn load_config( path: &str ) -> serde_yaml::Value {
+    let config_str = fs::read_to_string( path ).expect( "Error: Config file not found!" );
+    let config = serde_yaml::from_str( &config_str ).unwrap();
     config
 }
--- src/core/data_utils.rs
+++ src/core/data_utils.rs
@@ -3,10 +3,11 @@ use serde_yaml;
 
 
 
-pub(crate) struct DataGroupLayer {}
+
+pub(crate) struct DataGroupLayer;
 
 impl DataGroupLayer {
-    /// Walk on Hashmap-like based by keys (group_indexes) and records values and return mutable end-node of this way
+    /** Walk on Hashmap-like based by keys (group_indexes) and records values and return mutable end-node of this way */
     fn walk_by_keys<'a>(root_node: &'a mut serde_yaml::Value, group_indexes: &[String], rec: &HashMap<String, String>) -> &'a mut serde_yaml::Value {
             let mut curr_link = root_node;
             for k in group_indexes {
@@ -16,19 +17,19 @@ impl DataGroupLayer {
             curr_link
     }
 
-
+    /** Create nested map tree */
     pub(crate) fn create_nested_map(data_records: &[HashMap<String, String>], group_indexes: &[String]) -> serde_yaml::Value {
         let mut results = serde_yaml::Value::default();
-
         for rec in data_records {
-            let data = serde_yaml::to_value(rec).expect("Error: Failed convert value into XML-tree");
-            let mut node_link = Self::walk_by_keys(&mut results, &group_indexes, &rec);
+            let data = serde_yaml::to_value( rec ).expect( "Error: Failed convert value into XML-tree!" );
+            // let mut node_link = Self::walk_by_keys(&mut results, &group_indexes, &rec);
+            let node_link = Self::walk_by_keys(&mut results, &group_indexes, &rec);
             match node_link.is_null() {
                 true => {
-                    *node_link = serde_yaml::to_value(vec![data, ]).unwrap();
+                    *node_link = serde_yaml::to_value( vec![data,] ).unwrap();
                 },
                 false => {
-                    node_link.as_sequence_mut().unwrap().push(data);
+                    node_link.as_sequence_mut().unwrap().push( data );
                 }
             };
         }
