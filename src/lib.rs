7b601e4635688fdb6c561972d7c4f4ccfa0c7ca6
d.grigoriev <dmitry-gr87@yandex.ru>
2021-08-20 20:13:00 +0300

    Slightly refactoring and update dependencies (master 2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,49 @@
 #[macro_use] extern crate clap;
-extern crate grok;
-extern crate itertools;
-extern crate serde_yaml;
-extern crate serde_json;
-use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
-use serde_json::Value;
-use clap::App;
 
 use std::fs;
+use std::collections::HashMap;
+
+use clap::App;
+use serde::{ Serialize, Deserialize };
 
 mod core;
-use crate::core::paths_utils;
-use crate::core::config_utils;
-use crate::core::data_utils::{DataGroupLayer};
-use crate::core::user_types::{GroupedPathsType, SortedPathsType, SimpleMappingType, GroupedRecordsType};
+use crate::core::data_utils::DataGroupLayer;
+use crate::core::user_types::GroupedRecordsType;
+
 pub use crate::core::io_utils;
+pub use crate::core::config_utils;
+pub use crate::core::paths_utils;
+
 
 
 
 #[derive(Debug, Serialize, Deserialize)]
-struct AppResult {
+pub struct AppResult {
     order: Vec<String>,
     mapping: GroupedRecordsType
 }
 
 
-/// Get paths sordet by configuration file
-fn get_result(sorted_ids: Vec<String>, assets_mapping: GroupedRecordsType) -> AppResult {
+/** Get paths sorted by configuration file */
+pub fn get_result(sorted_ids: Vec<String>, assets_mapping: GroupedRecordsType) -> AppResult {
     AppResult { order: sorted_ids, mapping: assets_mapping }
 }
 
 
-/// Run module as function
-pub fn run(config: serde_json::Value, app_type: &str) -> serde_yaml::Value {
+/** Run module as function */
+pub fn run(config_path: &str, app_type: &str) -> serde_yaml::Value {
     // Load config objects from paths
-    let patterns: HashMap<String, String> = serde_json::from_value(config["custom_patterns"].clone()).unwrap();
+    let config = config_utils::load_config( config_path );
+    let patterns: HashMap<String, String> = serde_yaml::from_value( config["_custom_patterns"].clone() ).unwrap();
 
     // Get an important config fields
     let app_config = &config[app_type];
     let glob_patt = app_config["glob"].as_str().unwrap();
     let parsed_patt = app_config["parsed_patt"].as_str().unwrap();
-    let group_fields: Vec<String> = serde_json::from_value(app_config["group_fields"].clone()).unwrap();
-    let included_fields: Vec<String> = serde_json::from_value(app_config["included_fields"].clone()).unwrap();
+    let group_fields: Vec<String> = serde_yaml::from_value( app_config["group_fields"].clone() ).unwrap();
+    let included_fields: Vec<String> = serde_yaml::from_value( app_config["included_fields"].clone() ).unwrap();
 
-    let files = paths_utils::get_files(glob_patt);
+    let files = paths_utils::get_files( glob_patt );
     let records = paths_utils::parse_grok(included_fields, &parsed_patt, files, &patterns);
 
     DataGroupLayer::create_nested_map(&records, &group_fields)
@@ -52,18 +51,16 @@ pub fn run(config: serde_json::Value, app_type: &str) -> serde_yaml::Value {
 
 
 
-/// Run module as CLI
+/** Run module as CLI */
 pub fn run_cli() {
-    // let cli_config = load_yaml!("../cli.yml");
-    // let parser = App::from_yaml(cli_config).get_matches();
-    //
-    // let config_path = &parser.args["config"].vals[0].to_str().unwrap();
-    // let app_type = parser.args["type"].vals[0].to_str().unwrap();
-    //
-    // let results = run(config_path, app_type);
-    // fs::write("results.yml", serde_yaml::to_string(&results).unwrap());
-    //
-    // println!("{:?}", results);
+    let cli_config = load_yaml!( "../cli.yml" );
+    let parser = App::from_yaml( cli_config ).get_matches();
 
-    println!("START FINDER !");
+    let config_path = &parser.args["config"].vals[0].to_str().unwrap();
+    let app_type = parser.args["type"].vals[0].to_str().unwrap();
+
+    let results = run(config_path, app_type);
+    fs::write("results.yml", serde_yaml::to_string( &results ).unwrap()).unwrap();
+
+    println!("{:?}", results);
 }
